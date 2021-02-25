5decce64cfb12ea044b9825c3cc2463b24b0cd8f
ogonna-anaekwe <ogonna.anaekwe@gmail.com>
2021-02-25 13:55:18 -0500

    refactor: 💡 add transaction id

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import logging
+import uuid
 
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
@@ -14,7 +15,11 @@ def predict_sentiment(instances):
     Returns:
         reviews: a list of objects showing each review alongside a sentiment (i.e. positive or negative) and a sentiment score.
     """
-    try:
+    try:        
+        # transaction id
+        global transaction_id
+        transaction_id = uuid.uuid1()
+
         # tf serving rest endpoint
         port = '8501'
         base_url = f'http://localhost:{port}/v1/models/'
@@ -27,10 +32,10 @@ def predict_sentiment(instances):
         version = 2
         url = f'{base_url}{model_name}/versions/{version}:predict'
 
-        logging.info(f'serving version {version} of the text classifier model')
+        logging.info(f'{transaction_id}: serving version {version} of the text classifier model')
 
         total_reviews = len(instances)
-        logging.info(f'fetching sentiment for {total_reviews} {"reviews" if total_reviews > 1 else "review"}')
+        logging.info(f'{transaction_id}: fetching sentiment for {total_reviews} {"reviews" if total_reviews > 1 else "review"}')
         # construct data string and headers        
         data = json.dumps({"signature_name": "serving_default", "instances": instances})
         headers = {"content-type": "application/json"}
@@ -60,4 +65,4 @@ if __name__=='__main__':
     # update the review sample to test out different reviews
     review_samples = ['As others have mentioned, all the women that go nude in this film are mostly absolutely gorgeous. The plot very ably shows the hypocrisy of the female libido. When men are around they want to be pursued, but when no "men" are around, they become the pursuers of a 14 year old boy. And the boy becomes a man really fast (we should all be so lucky at this age!). He then gets up the courage to pursue his true love']    
     review_sentiment = predict_sentiment(review_samples)
-    logging.info(review_sentiment)
+    logging.info(f'{transaction_id}: {review_sentiment}')
