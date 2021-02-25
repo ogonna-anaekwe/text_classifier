6c87400e049a3023364455a934cc86f2a6a53bf8
ogonna-anaekwe <ogonna.anaekwe@gmail.com>
2021-02-25 17:44:08 -0500

    docs: ✏️ update logs in server script

--- bin/start_server.sh
+++ bin/start_server.sh
@@ -6,11 +6,11 @@ CONTAINER_NAME=`docker ps --format "{{.Names}}"`
 MODEL_FOLDER='/models/text_classifier'
 
 echo "running container named ${CONTAINER_NAME} in your current terminal session"
-echo "copying model config file into container named ${CONTAINER_NAME} in your current terminal session"
+echo "copying model.config file into container named ${CONTAINER_NAME} in your current terminal session"
 
 # copy model.config file into the model folder in the container so that it's visible to the container
 docker cp ./model.config ${CONTAINER_NAME}:${MODEL_FOLDER}
-echo "copied model config file into container named ${CONTAINER_NAME} in the ${MODEL_FOLDER} folder"
+echo "copied model.config file into container named ${CONTAINER_NAME} in the ${MODEL_FOLDER} folder"
 
 # stop container and rebuild with the recently copied model.config file
 docker stop ${CONTAINER_NAME}
